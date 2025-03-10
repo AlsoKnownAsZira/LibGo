bd7f10ffef9db3874ed97cd2386ed7cb8d476195
Brilyan Satria Wahyuda <brilyansatria10@gmail.com>
2025-03-10 18:00:05 +0700

    fix: addead simple animation to splash screen

--- lib/features/splash/view/ui/splash_screen.dart
+++ lib/features/splash/view/ui/splash_screen.dart
@@ -1,12 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:venturo_core/configs/routes/route.dart';
 import 'package:venturo_core/constants/image_constants.dart';
 import 'package:venturo_core/shared/styles/color_style.dart';
 import 'package:venturo_core/shared/styles/google_text_style.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+    _animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    );
+
+    _controller.forward().then((_) {
+      Get.offNamed(Routes.boardingRoute); 
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,14 +51,23 @@ class SplashScreen extends StatelessWidget {
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Center(child: Image(image: AssetImage(ImageConstant.logo))),
+              Center(
+                child: ScaleTransition(
+                  scale: _animation,
+                  child: const Image(image: AssetImage(ImageConstant.logo)),
+                ),
+              ),
               const SizedBox(height: 20),
-              Text(
-                'splash_welcome'.tr,
-                style: TextStyle(
+              ScaleTransition(
+                scale: _animation,
+                child: Text(
+                  'splash_welcome'.tr,
+                  style: TextStyle(
                     fontFamily: GoogleTextStyle.splash.fontFamily,
                     fontSize: 30.sp,
-                    color: ColorStyle.purple),
+                    color: ColorStyle.purple,
+                  ),
+                ),
               ),
             ],
           ),
